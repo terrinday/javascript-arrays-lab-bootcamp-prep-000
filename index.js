4584bd855f28fdf520666faddc68838f54675abf
Terrin Day <terrinday@gmail.com>
2018-09-14 23:21:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,9 +38,11 @@ function prependKitten(name)
 function removeLastKitten()
 {
   kittens.slice(0,kittens.length -1)
+  return kittens
 }
 
 function removeFirstKitten()
 {
   kittens.slice(1)
+  return kittens
 }
